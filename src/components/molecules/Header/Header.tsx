b4d17f5c8c37974b55fdc09eabd4bc25fc9fabe0
hidaken991018 <ed.hidaken@gmail.com>
2023-06-28 00:22:56 +0900

    feat: headerコンポーネント作成

---
+++ src/components/molecules/Header/Header.tsx
@@ -0,0 +1,13 @@
+import { ServiceLogo } from "@/components/atoms/ServiceLogo/ServiceLogo"
+import style from "./header.module.scss"
+import { NoticeButton } from "@/components/atoms/NoticeButton/NoticeButton"
+
+export const Header = () => {
+  // noticeCountはAPIから取得する
+  return (
+    <div className={style.header}>
+      <ServiceLogo />
+      <NoticeButton noticeCount={3} />
+    </div>
+  )
+}
