2659fc45564b5465752b59fabee1d9a931b07dc6
hidaken991018 <ed.hidaken@gmail.com>
2023-06-28 00:23:16 +0900

    feat: 共通ページへの適用

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
+import { Header } from '@/components/molecules/Header/Header'
+import { Sidebar } from '@/components/molecules/Sidebar/Sidebar'
+import './globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <div className="main">
+        <Sidebar />
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
