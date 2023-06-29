c5780e4dfdd1dff96b9d0773bf5a6b2fda0c0110
hidaken991018 <ed.hidaken@gmail.com>
2023-06-29 23:54:07 +0900

    feat : コンテンツカード作成

---
+++ src/components/molecules/ContentCard/ContentCard.tsx
@@ -0,0 +1,33 @@
+import { ContentDescription } from "@/components/atoms/ContentDescription/ContentDescription"
+import { FollowButton } from "@/components/atoms/FollowButton/FollowButton"
+import { TimelineProfile } from "@/components/atoms/TimelineProfile/TimelineProfile"
+import Image from "next/image"
+import style from "./content_card.module.scss"
+type Props = {
+  profile: {
+    name: string,
+    isFollowed: boolean,
+  },
+  content: {
+    title: string,
+    description: string,
+    image: string,
+  }
+}
+
+export const ContentCard = (props: Props) => {
+  return (
+    <div className={style.content}>
+      <div className={style.content_header}>
+        <TimelineProfile name={props.profile.name} />
+        <FollowButton isFollowed={props.profile.isFollowed} />
+      </div>
+      <div className={style.image_wrapper}>
+        <Image src={props.content.image} alt={props.content.title} width={600} height={260} />
+      </div>
+      <div className={style.content_deescription}>
+        <ContentDescription title={props.content.title} description={props.content.description} />
+      </div>
+    </div>
+  )
+}
